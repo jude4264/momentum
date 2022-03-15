34a083b63f54dffdcd077cf3126bb545d0d7d711
sangmi <jude4264@gmail.com>
2022-03-15 15:33:49 +0900

    [ADD] ToDo 리스트 삭제

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,26 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
+function deleteToDO(event){
+    // console.log(event.target);
+    //console.log(event.target.parentElement);
+    // console.dir(event.target);
+    // console.dir(event.target.parentElement.innerText);
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+
 function paintToDo(newTodo){
     // console.log("i will paint " + newTodo);
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click" , deleteToDO)
+    li.appendChild(span);
+    li.appendChild(button);
     // console.log(li);
     todoList.appendChild(li);
 }
