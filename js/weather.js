38c8d115a40b27ab90de2f264c1d119cfc3038ee
sangmi <jude4264@gmail.com>
2022-03-16 15:20:04 +0900

    [ADD] 현재날씨 추가

---
+++ js/weather.js
@@ -0,0 +1,28 @@
+const API_KEY = "eb0029f4c28cbca69da7db15a5dd29ea";
+
+function onGeoOk(position){
+
+    const lat =  position.coords.latitude;
+    const lon =  position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+
+    // console.log("You live in",lat,lon);
+    // console.log(url);
+    fetch(url).then(response => response.json()).then(data =>{
+
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+
+
+    });
+
+}
+
+function onGeoErr(){
+    alert("Can't find you. No weather for you.");
+}
+
+
+navigator.geolocation.getCurrentPosition(onGeoOk,onGeoErr);
